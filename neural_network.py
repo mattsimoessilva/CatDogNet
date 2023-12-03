f61b52a546657e180624335ec8271ca819ab071a
Matheus da Silva <matheussimoesdasilva2002@gmail.com>
2023-12-03 17:19:23 -0300

    Switching to numpy functions

--- neural_network.py
+++ neural_network.py
@@ -1,14 +1,12 @@
-from math_functions import dot_product, matrix_addition, scalar_multiplication, transpose
-import math
-import random
+import numpy as np
 
 class NeuralNetwork:
     def __init__(self, n_x, n_h, n_y, alpha=0.01, batch_size=32, epochs=100, lambd=0.7):
         # Initialize weights (W), biases (b) and hyperparameters
-        self.W1 = [[random.uniform(-1, 1) for _ in range(n_h)] for _ in range(n_x)]  # Use a better weight initialization
-        self.b1 = [[0] for _ in range(n_h)]
-        self.W2 = [[random.uniform(-1, 1) for _ in range(n_y)] for _ in range(n_h)]
-        self.b2 = [[0] for _ in range(n_y)]
+        self.W1 = np.random.uniform(-1, 1, size=(n_x, n_h))
+        self.b1 = np.zeros((1, n_h))
+        self.W2 = np.random.uniform(-1, 1, size=(n_h, n_y))
+        self.b2 = np.zeros((1, n_y))
         self.alpha = alpha
         self.batch_size = batch_size
         self.epochs = epochs
@@ -16,62 +14,60 @@ class NeuralNetwork:
 
     def relu(self, Z):
         # ReLU activation function
-        return [[max(0, z) for z in row] for row in Z]
+        return np.maximum(0, Z)
 
     def relu_derivative(self, Z):
         # Derivative of ReLU function for backpropagation
-        return [[int(z > 0) for z in row] for row in Z]
+        return (Z > 0).astype(int)
 
     def softmax(self, Z):
         # Softmax activation function
-        epsilon = 1e-8  # Small constant to avoid division by zero
-        expZ = [[math.exp(z) for z in row] for row in Z]
-        sum_expZ = sum([sum(row) for row in expZ]) + epsilon
-        return [[z / sum_expZ for z in row] for row in expZ]
+        epsilon = 1e-8
+        expZ = np.exp(Z)
+        return expZ / np.sum(expZ, axis=1, keepdims=True)
 
     def forward_propagation(self, X):
         # Compute the output
-        self.Z1 = matrix_addition(dot_product(X, self.W1), self.b1)
+        self.Z1 = X.dot(self.W1) + self.b1
         self.A1 = self.relu(self.Z1)
-        self.Z2 = matrix_addition(dot_product(self.A1, self.W2), self.b2)
+        self.Z2 = self.A1.dot(self.W2) + self.b2
         self.A2 = self.softmax(self.Z2)
         return self.A2
 
     def compute_cost(self, A, Y):
         # Cross-entropy cost function with L2 regularization
-        m = len(Y[0])
-        epsilon = 1e-8  # Small constant to avoid division by zero
-        cost = -sum([sum([y * math.log(max(a, epsilon)) for a, y in zip(row_A, row_Y)]) for row_A, row_Y in zip(A, Y)]) / m
-        L2_regularization_cost = (sum([sum([w**2 for w in row]) for row in self.W1]) + sum([sum([w**2 for w in row]) for row in self.W2])) * self.lambd / (2 * m)
+        m = len(Y)
+        epsilon = 1e-8
+        cost = -np.sum(Y * np.log(np.maximum(A, epsilon))) / m
+        L2_regularization_cost = (np.sum(self.W1**2) + np.sum(self.W2**2)) * self.lambd / (2 * m)
         return cost + L2_regularization_cost
 
     def backward_propagation(self, X, Y):
         # Compute the error and adjust weights & biases
-        m = len(X[0])
-        dZ2 = matrix_addition(self.A2, scalar_multiplication(-1, Y))
-        dW2 = scalar_multiplication(1 / m, dot_product(transpose(self.A1), dZ2))
-        db2 = scalar_multiplication(1 / m, [sum(row) for row in transpose(dZ2)])
-        dZ1 = dot_product(dZ2, transpose(self.W2))
-        dW1 = scalar_multiplication(1 / m, dot_product(transpose(X), dZ1))
-        db1 = scalar_multiplication(1 / m, [sum(row) for row in transpose(dZ1)])
+        m = len(X)
+        dZ2 = self.A2 - Y
+        dW2 = (self.A1.T.dot(dZ2) + self.lambd * self.W2) / m
+        db2 = np.sum(dZ2, axis=0, keepdims=True) / m
+        dZ1 = dZ2.dot(self.W2.T) * self.relu_derivative(self.Z1)
+        dW1 = (X.T.dot(dZ1) + self.lambd * self.W1) / m
+        db1 = np.sum(dZ1, axis=0, keepdims=True) / m
 
         # Update the weights and biases
-        self.W1 = matrix_addition(self.W1, scalar_multiplication(-self.alpha, dW1))
-        self.b1 = matrix_addition(self.b1, scalar_multiplication(-self.alpha, db1))
-        self.W2 = matrix_addition(self.W2, scalar_multiplication(-self.alpha, dW2))
-        self.b2 = matrix_addition(self.b2, scalar_multiplication(-self.alpha, db2))
+        self.W1 -= self.alpha * dW1
+        self.b1 -= self.alpha * db1
+        self.W2 -= self.alpha * dW2
+        self.b2 -= self.alpha * db2
 
     def train(self, X, Y):
         # Training loop with mini-batch gradient descent
-        m = len(X[0])
-        permutation = list(range(m))
-        random.shuffle(permutation)
-        shuffled_X = [X[i] for i in permutation]
-        shuffled_Y = [Y[i] for i in permutation]
+        m = len(X)
+        permutation = np.random.permutation(m)
+        shuffled_X = X[permutation]
+        shuffled_Y = Y[permutation]
 
         for epoch in range(self.epochs):
             epoch_cost = 0
-            num_batches = int(m / self.batch_size)
+            num_batches = m // self.batch_size
             for i in range(num_batches):
                 start = i * self.batch_size
                 end = start + self.batch_size
@@ -90,5 +86,4 @@ class NeuralNetwork:
     def predict(self, X):
         # Predict the label of an unseen image
         A2 = self.forward_propagation(X)
-        return [row.index(max(row)) for row in A2]
-
+        return np.argmax(A2, axis=1)
