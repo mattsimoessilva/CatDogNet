d709339a33116b5c716f093f77c378f227605257
Matheus da Silva <matheussimoesdasilva2002@gmail.com>
2023-12-03 18:32:10 -0300

    Adding value check to train.py

--- train.py
+++ train.py
@@ -11,6 +11,11 @@ n_x = len(X_train[0])
 n_h = 64  # Number of neurons in the hidden layer
 n_y = len(set(Y_train))  # Number of classes (assuming classification)
 
+# Checking values
+print(f"n_x = {n_x}")
+print(f"n_h = {n_h}")
+print(f"n_y = {n_y}")
+      
 neural_net = NeuralNetwork(n_x, n_h, n_y, alpha=0.01, batch_size=32, epochs=100, lambd=0.7)
 neural_net.train(X_train, Y_train)
 
