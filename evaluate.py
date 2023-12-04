423e4a95446abe25d723209da49efe8d20aba8f5
Matheus da Silva <matheussimoesdasilva2002@gmail.com>
2023-12-04 06:49:35 -0300

    Fixing training again

--- evaluate.py
+++ evaluate.py
@@ -1,20 +1,31 @@
 import tensorflow as tf
+from keras.preprocessing import image
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
 import numpy as np
+from keras.applications.vgg16 import decode_predictions
 
-# Load the image
-img = tf.keras.preprocessing.image.load_img('teste3.jpeg', target_size=(64, 64))
-img = tf.keras.preprocessing.image.img_to_array(img)
-img = np.expand_dims(img, axis=0)
+# load the VGG16 model with pre-trained ImageNet weights
+model = VGG16(weights='imagenet', include_top=True)
 
-# Load the model
-model = tf.keras.models.load_model('model.h5')
+# load the image
+img_path = 'teste2.jpeg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
 
-# Evaluate the model
-predictions = model.predict(img)
+# predict the probability across all output classes
+y_prob = model.predict(x) 
+y_classes = y_prob.argmax(axis=-1)
 
-# Print the prediction
-if np.argmax(predictions) == 0:
-    print('The model predicts this image is a cat.')
-else:
-    print('The model predicts this image is a dog.')
+print(y_classes)
+
+# convert the probabilities to class labels
+label = decode_predictions(y_prob)
+# retrieve the most likely result, e.g. highest probability
+label = label[0][0]
+# print the classification
+print('%s (%.2f%%)' % (label[1], label[2]*100))
+
 
