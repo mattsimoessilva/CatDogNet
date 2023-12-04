dcf33b18096291caeca3395f619364410c723157
Matheus da Silva <matheussimoesdasilva2002@gmail.com>
2023-12-04 07:18:43 -0300

    Fixing file paths

--- process.py
+++ process.py
@@ -5,7 +5,7 @@ from random import seed
 from random import random
 
 # create directories
-subdirs = ['train/train/', 'test/test/']
+subdirs = ['train/train', 'test/test']
 for subdir in subdirs:
 	# create label subdirectories
 	labeldirs = ['dogs/', 'cats/']
@@ -18,12 +18,12 @@ seed(1)
 # define ratio of pictures to use for validation
 val_ratio = 0.25
 # copy training dataset images into subdirectories
-src_directory = 'train/train/'
+src_directory = 'train/train'
 for file in listdir(src_directory):
 	src = src_directory + '/' + file
-	dst_dir = 'train/train/'
+	dst_dir = 'train/train'
 	if random() < val_ratio:
-		dst_dir = 'test/test/'
+		dst_dir = 'test/test'
 	if file.startswith('cat'):
 		dst = dst_dir + 'cats/'  + file
 		copyfile(src, dst)
